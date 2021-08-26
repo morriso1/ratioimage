1d179377aa7629c2eb07766d7e31d6366e74bc93
morriso1 <ottomorris@gmail.com>
2021-08-26 15:39:15 +0100

    added exports

--- ratioimage/_nbdev.py
+++ ratioimage/_nbdev.py
@@ -3,10 +3,16 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"create_img_dict_from_folder": "00_ratio_images.ipynb",
+         "create_ratio_image": "00_ratio_images.ipynb",
+         "create_dict_of_ratio_images": "00_ratio_images.ipynb",
          "measure_region_props_to_tidy_df": "00_ratio_images.ipynb",
-         "ratiometric_measure_region_props_to_tidy_df": "00_ratio_images.ipynb"}
+         "only_intensity_region_properties": "00_ratio_images.ipynb",
+         "ratiometric_measure_region_props_to_tidy_df": "00_ratio_images.ipynb",
+         "split_img_key_col_to_sample_id_and_replicates": "00_ratio_images.ipynb",
+         "sample_id_to_categories": "00_ratio_images.ipynb"}
 
-modules = ["ratio_images.py"]
+modules = ["ratio_images.py",
+           "plot_results.py"]
 
 doc_url = "https://morriso1.github.io/ratioimage/"
 
